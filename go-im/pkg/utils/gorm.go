5e04ab076947924a2c01757c00d9e7971838e7d3
wanghaoran20 <wanghaoran20@zuoyebang.com>
2026-05-24 00:14:54 +0800

    数据库连接池

--- go-im/pkg/utils/gorm.go
+++ go-im/pkg/utils/gorm.go
@@ -1,16 +1,26 @@
 package utils
 
 import (
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB(dsn string) *gorm.DB {
-	var err error
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	return db
 }
